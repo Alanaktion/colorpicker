5899b972575b4603c8e20dd82bbc4ed4cb3d6304
Alan Hardman <alanaktion@gmail.com>
2019-10-23 16:59:17 -0600

    Setting up basic dependencies

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import Button from './Button';
 
+import chroma from 'chroma-js';
+
 export default class App extends React.Component {
+  click(e) {
+    console.log(e);
+  }
+
+  componentDidMount() {
+    this.setState({
+      baseColor: chroma.scale('GnBu').colors(1)[0],
+    });
+  }
+
   render() {
     return (
       <div className="px-4 py-2">
         <h1 className="text-2xl">{this.props.name}</h1>
-        <Button>button</Button>
+        <Button className="mr-2">button</Button>
         <Button primary>primary button</Button>
       </div>
     );
--- src/components/Button.js
+++ src/components/Button.js
@@ -6,8 +6,10 @@ export default class Button extends React.Component {
     if (this.props.primary) {
       color = 'bg-blue-500 hover:bg-blue-700 text-white';
     }
+    let className = color + ' font-bold py-2 px-4 rounded ';
+    className += this.props.className;
     return (
-      <button type="button" className={color + ' font-bold py-2 px-4 rounded ' + this.props.className}>
+      <button type="button" onClick={this.props.onClick} className={className}>
         {this.props.children}
       </button>
     );
